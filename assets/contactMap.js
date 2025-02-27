ff76b5e4f25132ee966a3eedce5b9c357bd87720
Augustin Viard <gugusviard@gmail.com>
2025-02-27 18:41:20 +0100

    fixed h elements

--- assets/contactMap.js
+++ assets/contactMap.js
@@ -6,10 +6,10 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 const popup = L.popup([44.8465743,-0.6065365], {
     content: `<ul>
-		<li><h1>Informations de contact</h1></li>
+		<li><p class="footer-title">Informations de contact</p></li>
 		<li><span>Téléphone :</span><p>+33 5 56 67 78 89</p></li>
 		<li><span>Adresse :</span><p><a href="https://maps.app.goo.gl/i1vVoqTwm5fPUwQU8" target="_blank">68 Av. Alsace Lorraine, 332000 Bordeaux</a></p></li>
-		<li><h1>Horaires :</h1></li>
+		<li><p class="footer-title">Horaires :</p></li>
 		<ul>
 			<li><span>Lundi-samedi :</span><p>08:00-18:00</p></li>
 			<li><span>Dimanche :</span><p>Fermé</p></li>
